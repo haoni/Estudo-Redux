a1a9851a56199d79548e7cc4e40854ba82084c22
haoni <haoni@outlook.com>
2019-04-23 17:18:08 -0400

    Incluindo os arquivos

--- estudo-redux/src/components/custon-input.js
+++ estudo-redux/src/components/custon-input.js
@@ -3,6 +3,6 @@ import React from 'react'
 
 export default props => {
     return (
-        <input id={props._id}  placeholder={props.placeholder} value={props.value} onChange={props.onChange}/>
+        <input id={props._id} placeholder={props.placeholder} value={props.value} onChange={props.onChange}/>
     )
 }
--- estudo-redux/src/index.js
+++ estudo-redux/src/index.js
@@ -4,9 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {combineReducers, createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const reducers = combineReducers({
+    field: () => ({value: 'Opa!'})    
+})
+
+ReactDOM.render(
+<Provider store={createStore(reducers)}>
+    <App />
+</Provider>
+    
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+    
